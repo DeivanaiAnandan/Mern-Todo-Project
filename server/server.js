e5e5f768b96c72f1a2edc978f8ac64d2f5385326
DeivanaiAnandan <deivanai.anandan@gmail.com>
2025-12-16 13:00:16 +0530

    Route commit

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import todoRoutes from './Routes/todoRoutes.js';
-import { connectDB } from './config/db.js';
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import todoRoutes from "./routes/todoRoutes.js";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 // Handle ESM __dirname issue
@@ -19,7 +19,7 @@ const app = express();
 // })
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/todos', todoRoutes)
+app.use("/api/todos", todoRoutes);
 
 // // Serve frontend
 // if (process.env.NODE_ENV === 'production') {
@@ -34,20 +34,18 @@ app.use('/api/todos', todoRoutes)
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(
-    express.static(path.join(__dirname, "..", "client", "build"))
-  );
+  app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
   app.get(/.*/, (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "..", "client", "build", "index.html")
-    )
+    res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => res.send("API running"));
 }
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running in ${process.env.NODE_ENV} mode and in ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode and in ${PORT}`
+  );
+});
